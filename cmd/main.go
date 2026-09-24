4218a796db7fd1a1757519c994069590be8622f1
Hrishabh Joshi <hrishabh.joshi@epifi.com>
2026-09-24 11:06:05 +0530

    clean

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,1 @@
 package main
-
-import (
-	"fmt"
-	DBManager "go-base-project/db"
-	"go-base-project/services/incident"
-)
-
-func main() {
-	fmt.Println("Starting of the program")
-
-	db := DBManager.NewDB()
-
-	incidentService := incident.NewIncidentService(db)
-
-}
